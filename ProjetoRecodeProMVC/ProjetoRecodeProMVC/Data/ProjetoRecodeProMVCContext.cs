8545614fce126964f865591cf2d3d884553ecc80
MagnoBarbosaLima <magnobarbosaliima@gmail.com>
2021-12-20 21:24:41 -0300

    first commit

---
+++ ProjetoRecodeProMVC/ProjetoRecodeProMVC/Data/ProjetoRecodeProMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoRecodeProMVC.Models;
+
+namespace ProjetoRecodeProMVC.Data
+{
+    public class ProjetoRecodeProMVCContext : DbContext
+    {
+        public ProjetoRecodeProMVCContext (DbContextOptions<ProjetoRecodeProMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoRecodeProMVC.Models.Cliente> Cliente { get; set; }
+    }
+}
---
+++ ProjetoRecodeProMVC/ProjetoRecodeProMVC/Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoRecodeProMVC.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CPF { get; set;}
+        public string Destinos { get; set;}
+
+
+    }
+}
